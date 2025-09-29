d6cfb4deb3b27a38bdd00403340ad8dd750e160f
12999441 <12999441@itisrossi.vi.it>
2025-09-29 13:04:53 +0200

    finito

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    char str[] = "i topi non avevano nipoti";
+    char frase[] = "i topi non avevano nipoti";
+    int lunghezza = 0;
+
+    while (frase[lunghezza]!='\0'){ lunghezza++; } //trova lunghezza string
+
+    char *inizio = frase, *fine = frase + lunghezza-1;
+
+    for (int i = 0; i < lunghezza/2; i++) {
+        while (*inizio == ' '){ inizio++; }
+        while (*fine == ' '){ fine--; }
+
+        if (*inizio != *fine) {
+            printf("La frase non e' palindroma");
+            return 0;
+        }
+        inizio++;
+        fine--;
+    }
+    printf("La frase e' palindroma");
+
     return 0;
 }
